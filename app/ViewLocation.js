f31a56f9da1a7ff50417e6921818549265a6e971
codelab265 <mphatsomlenga1@gmail.com>
2023-12-02 06:13:17 +0200

    location

--- app/ViewLocation.js
+++ app/ViewLocation.js
@@ -1,7 +1,13 @@
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigation, useLocalSearchParams } from "expo-router";
-import MapView, { Circle, Marker, PROVIDER_GOOGLE, Polygon } from "react-native-maps";
+import MapView, {
+  Circle,
+  Marker,
+  PROVIDER_GOOGLE,
+  Polygon,
+  Polyline,
+} from "react-native-maps";
 
 import { Appbar, Button, Menu } from "react-native-paper";
 import * as Location from "expo-location";
@@ -17,11 +23,18 @@ const ViewLocation = () => {
   const map = useRef(null);
   const [MenuVisible, setMenuVisible] = useState(false);
   const GOOGLE_MAPS_APIKEY = "AIzaSyAQrBDvwyt-UZyeTQFmO52xVf5Iu23Qoqw";
-  const origin = { latitude: location?.coords.latitude, longitude: location?.coords.longitude };
-  const destination = { latitude: parseFloat(report.report_detail[0]?.latitude), longitude: parseFloat(report.report_detail[0]?.longitude) };
+  const origin = {
+    latitude: location?.coords.latitude,
+    longitude: location?.coords.longitude,
+  };
+  const destination = {
+    latitude: parseFloat(report.report_detail[0]?.latitude),
+    longitude: parseFloat(report.report_detail[0]?.longitude),
+  };
 
   const [windDirection, setWindDirection] = useState(null);
   const [windSpeed, setWindSpeed] = useState(null);
+  const [spreadDistance, setSpreadDistance] = useState(null);
 
   const openViewAPI = "c14e51f1f5fb7d1491352f44a1788115";
 
@@ -36,9 +49,7 @@ const ViewLocation = () => {
     });
   };
 
-  
   useEffect(() => {
-    // Function to fetch wind direction data from OpenWeatherMap
     const fetchWindDirection = async () => {
       try {
         const response = await axios.get(
@@ -49,24 +60,27 @@ const ViewLocation = () => {
         const newWindDirection = windData.deg;
         const newWindSpeed = windData.speed;
 
-        // Update the wind direction and speed states
         setWindDirection(newWindDirection);
         setWindSpeed(newWindSpeed);
         console.log(windData);
+
+        const rateOfSpread = 5;
+        const projectedTime = 3600;
+        const newSpreadDistance = rateOfSpread * projectedTime;
+
+        setSpreadDistance(newSpreadDistance);
       } catch (error) {
-        console.error('Error fetching wind data:', error);
+        console.error("Error fetching wind data:", error);
       }
     };
 
     // Initial fetch
     fetchWindDirection();
 
-    // Set up a timer to periodically update wind direction
-    const interval = setInterval(fetchWindDirection, 60000); // Refresh every 60 seconds
+    const interval = setInterval(fetchWindDirection, 60000);
 
-    return () => clearInterval(interval); // Cleanup on unmount
+    return () => clearInterval(interval);
   }, []);
-
 
   useEffect(() => {
     (async () => {
@@ -82,6 +96,89 @@ const ViewLocation = () => {
       setLoading(false);
     })();
   }, []);
+
+  const calculatePolygonCoordinates = () => {
+    const earthRadius = 299478139;
+    const latDelta = (spreadDistance / earthRadius) * (180 / Math.PI);
+    const lonDelta =
+      (spreadDistance /
+        (earthRadius * Math.cos((Math.PI / 180) * destination.latitude))) *
+      (180 / Math.PI);
+
+    const topLeft = {
+      latitude: destination.latitude + latDelta,
+      longitude: destination.longitude - lonDelta,
+    };
+    const topRight = {
+      latitude: destination.latitude + latDelta,
+      longitude: destination.longitude + lonDelta,
+    };
+    const bottomRight = {
+      latitude: destination.latitude - latDelta,
+      longitude: destination.longitude + lonDelta,
+    };
+    const bottomLeft = {
+      latitude: destination.latitude - latDelta,
+      longitude: destination.longitude - lonDelta,
+    };
+
+    return [topLeft, topRight, bottomRight, bottomLeft];
+  };
+
+  const calculateSpreadDirectionCoordinates = () => {
+    const spreadDirection = windDirection;
+    const spreadDirectionLength = spreadDistance * 2;
+
+    const spreadDirectionStart = {
+      latitude: destination.latitude,
+      longitude: destination.longitude,
+    };
+
+    const spreadDirectionEnd = {
+      latitude:
+        destination.latitude +
+        spreadDirectionLength * Math.sin((spreadDirection * Math.PI) / 180),
+      longitude:
+        destination.longitude +
+        spreadDirectionLength * Math.cos((spreadDirection * Math.PI) / 180),
+    };
+
+    return [spreadDirectionStart, spreadDirectionEnd];
+  };
+
+  const calculateArrowShapeCoordinates = () => {
+    const arrowWidth = spreadDistance / 5; // Adjust the width of the arrow as needed
+    const arrowLength = spreadDistance * 2; // Adjust the length of the arrow as needed
+
+    const arrowHead = {
+      latitude:
+        destination.latitude +
+        arrowLength * Math.sin((windDirection * Math.PI) / 180),
+      longitude:
+        destination.longitude +
+        arrowLength * Math.cos((windDirection * Math.PI) / 180),
+    };
+
+    const arrowLeft = {
+      latitude:
+        arrowHead.latitude -
+        arrowWidth * Math.cos(((windDirection + 90) * Math.PI) / 180),
+      longitude:
+        arrowHead.longitude -
+        arrowWidth * Math.sin(((windDirection + 90) * Math.PI) / 180),
+    };
+
+    const arrowRight = {
+      latitude:
+        arrowHead.latitude +
+        arrowWidth * Math.cos(((windDirection + 90) * Math.PI) / 180),
+      longitude:
+        arrowHead.longitude +
+        arrowWidth * Math.sin(((windDirection + 90) * Math.PI) / 180),
+    };
+
+    return [arrowLeft, arrowHead, arrowRight];
+  };
 
   return (
     <View className="flex-1 relative">
@@ -119,7 +216,7 @@ const ViewLocation = () => {
             }}
             title="Chat"
           />
-          
+
           <Menu.Item
             onPress={() => {
               closeMenu();
@@ -149,27 +246,84 @@ const ViewLocation = () => {
           provider={PROVIDER_GOOGLE}
           ref={map}
         >
-          <Marker
-            coordinate={destination}
-            pinColor="green"
+          <Marker coordinate={destination} pinColor="green" />
+          <Circle
+            center={destination}
+            radius={300}
+            fillColor="rgba(255, 0, 0, 0.3)"
           />
-          <Circle center={destination} radius={300}
-          fillColor="rgba(255, 0, 0, 0.3)"/>
           <MapViewDirections
             origin={origin}
             destination={destination}
-            apikey={GOOGLE_MAPS_APIKEY} // insert your API Key here
+            apikey={GOOGLE_MAPS_APIKEY}
             strokeWidth={5}
             strokeColor="green"
           />
+
+          {windDirection !== null && (
+            <Polygon
+              coordinates={calculatePolygonCoordinates()}
+              fillColor="rgba(255, 0, 0, 0.3)"
+              strokeColor="red"
+            />
+          )}
+
+          {windDirection !== null && (
+            <Polygon
+              coordinates={calculateArrowShapeCoordinates()}
+              fillColor="rgba(255, 0, 0, 0.3)"
+              strokeColor="red"
+            />
+          )}
+
+          {/* Display wind information on the map using the WindInfo component
+          {windDirection !== null && windSpeed !== null && (
+            <View style={styles.windInfoContainer}>
+              <Text style={styles.windInfoText}>
+                Wind Direction: {windDirection}°
+              </Text>
+              <Text style={styles.windInfoText}>
+                Wind Speed: {windSpeed} m/s
+              </Text>
+            </View>
+          )} */}
+
+          {/* Display a marker for the fire location */}
+          <Marker
+            coordinate={destination}
+            title="Fire Location"
+            pinColor="red"
+          />
+
+          {/* Display a line representing the fire spread direction */}
+          {/* {windDirection !== null && (
+            <Polyline
+              coordinates={calculateSpreadDirectionCoordinates()}
+              strokeColor="orange"
+              strokeWidth={2}
+            />
+          )} */}
         </MapView>
       )}
       {windDirection !== null && windSpeed !== null && (
         <WindInfo windDirection={windDirection} windSpeed={windSpeed} />
       )}
-      
     </View>
   );
 };
 
 export default ViewLocation;
+
+const styles = StyleSheet.create({
+  windInfoContainer: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    padding: 10,
+    borderRadius: 5,
+  },
+  windInfoText: {
+    fontSize: 14,
+  },
+});
